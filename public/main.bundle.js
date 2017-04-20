e7937343763951ae4a84c073696feab846d8c27a
alxgeoaln <georgianalinalexandru@gmail.com>
2017-04-20 20:05:00 +0300

    router

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -1973,7 +1973,9 @@ var EventDetailsComponent = (function () {
     EventDetailsComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.route.params.forEach(function (params) {
-            _this.event = _this.eventService.getEvent(+params['event']);
+            _this.event = _this.eventService.getEvent(console.log(params)
+            // +params['event']
+            );
         });
         this.addSession = true;
         // this.event = this.route.snapshot.data['event'];
