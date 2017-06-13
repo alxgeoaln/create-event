8dbd06474ccb3af9250481b46fe66bcc3ae3f094
alxgeoaln <georgianalinalexandru@gmail.com>
2017-06-13 17:09:23 +0300

    Create database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://galin:Mcwtuyokzmn1@ds047166.mlab.com:47166/create-event',
+    database: 'mongodb://galin:xxxxx.mlab.com:47166/create-event',
     secret: 'yoursecret'
 };
